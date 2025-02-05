414d47377ac5079e925a666eec36b012011e7e56
NelsonNV <nelsonluisnv2@gmail.com>
2025-02-04 23:16:53 -0300

    feat: modo debug con icecream

--- DownPlugins.lua
+++ DownPlugins.lua
@@ -1,5 +1,15 @@
 #!/usr/bin/env lua
 
+local ic = require("icecream")
+
+local debug = false
+
+for _, v in pairs(arg) do
+	if v == "--debug" then
+		debug = true
+	end
+end
+
 local DicionariPlugin = {
 	autovirtualenv = "MichaelAquilina/zsh-autoswitch-virtualenv",
 	syntaxcheck = "zsh-users/zsh-syntax-highlighting",
@@ -10,6 +20,9 @@ local DicionariPlugin = {
 for plugin, url in pairs(DicionariPlugin) do
 	print("Installing " .. plugin)
 	local dirOutPlugins = string.format("~/.zshp/%s", plugin)
-	local cmd = string.format("git clone %s %s", url, dirOutPlugins)
-	os.execute(cmd)
+	local cmd = string.format("git clone https//:github.com/%s %s", url, dirOutPlugins)
+	ic(cmd)
+	if not debug then
+		os.execute(cmd)
+	end
 end
